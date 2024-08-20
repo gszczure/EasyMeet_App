a75d61ab949e677cde0d6c5ed4279524ddd798de
gszczure <grzegorz.sz1202@outlook.com>
2024-08-20 19:26:25 +0200

    config chane name for endpoint

--- src/main/java/pl/meetingapp/backendtest/backend/model/MeetingRequest.java
+++ src/main/java/pl/meetingapp/backendtest/backend/model/MeetingRequest.java
@@ -3,6 +3,7 @@ package pl.meetingapp.backendtest.backend.model;
 public class MeetingRequest {
 
     // Klasa DTO ktora z JSON wybiera tylko name po to by potem zapisac je w tabeli meetings
+    // oraz do pobierania code i uzywania go w /join
     private String name;
     private String code;
 
