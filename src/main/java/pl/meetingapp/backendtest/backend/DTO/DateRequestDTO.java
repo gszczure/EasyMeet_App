8a6f4c7564d436811c75ad97551452351fd39dce
gszczure <grzegorz.sz1202@outlook.com>
2024-09-10 18:08:31 +0200

    Adding DataRequestDTO

---
+++ src/main/java/pl/meetingapp/backendtest/backend/DTO/DateRequestDTO.java
@@ -0,0 +1,13 @@
+package pl.meetingapp.backendtest.backend.DTO;
+
+public class DateRequestDTO {
+    private String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
