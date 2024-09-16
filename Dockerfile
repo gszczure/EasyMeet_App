eba8c429a8ec54a45b4103562f82304d49266ff1
gszczure <grzegorz.sz1202@outlook.com>
2024-09-16 17:35:42 +0200

    Adding Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM openjdk:17-jdk
+
+ARG JAR_FILE=target/BACKENDTEST-0.0.1-SNAPSHOT.jar
+
+COPY ${JAR_FILE} app.jar
+
+ENV PORT 8080
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/test/java/pl/meetingapp/backendtest/BackendtestApplicationTests.java
+++ src/test/java/pl/meetingapp/backendtest/BackendtestApplicationTests.java
@@ -1,13 +1,14 @@
-package pl.meetingapp.backendtest;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BackendtestApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package pl.meetingapp.backendtest;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import pl.meetingapp.backendtest.backend.BackendApplication;
+//
+//@SpringBootTest(classes = BackendApplication.class)
+//class BackendApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
